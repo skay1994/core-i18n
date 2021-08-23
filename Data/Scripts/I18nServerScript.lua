45b0680b57523829b7ca4e86fe6983b3fb01b8e8
Jorge Carlos <skay_1994@yahoo.com.br>
2021-08-23 12:39:14 -0300

    Added Shared Storage

--- Data/Scripts/I18nServerScript.lua
+++ Data/Scripts/I18nServerScript.lua
@@ -1,17 +1,37 @@
-local propDefaultLanguage = script:GetCustomProperty("DefaultLanguage")
+local propSharedStorageKey = script:GetCustomProperty("SharedStorageKey")
+local propDefaultLocale = script:GetCustomProperty("DefaultLocale")
+
+local storageKey = "i18n_current_locale"
+
+function GetPlayerStorage(player)
+    if propSharedStorageKey then
+        return Storage.GetSharedPlayerData(propSharedStorageKey, player)
+    end
+
+    return Storage.GetPlayerData(player)
+end
+
+function SetPlayerStorage(player, storage)
+    if propSharedStorageKey then
+        return Storage.SetSharedPlayerData(propSharedStorageKey, player, storage)
+    end
+
+    Storage.SetPlayerData(player, storage)
+end
 
 function UpdatePlayerLocale(player, locale)
-    local storage = Storage.GetPlayerData(player)
-    storage["current_locale"] = locale
-    Storage.SetPlayerData(player, storage)
+    local storage = GetPlayerStorage(player)
+    storage[storageKey] = locale
+    SetPlayerStorage(player, storage)
     print(("Updated locale to %q"):format(player.name))
 end
-function OnPlayerJoined(player)
-    local storage = Storage.GetPlayerData(player)
-    local locale = propDefaultLanguage
 
-    if storage["current_locale"] and storage["current_locale"] ~= ""  then
-        locale = storage["current_locale"]
+function OnPlayerJoined(player)
+    local storage = GetPlayerStorage(player)
+    local locale = propDefaultLocale
+
+    if storage[storageKey] and storage[storageKey] ~= ""  then
+        locale = storage[storageKey]
     end
 
     Events.BroadcastToPlayer(player, "I18N_Connect", locale)
